514830ee75ba00c93ff35298ae7a2672bd0de67d
Delaney Lehouillier <dlehouillier@hotmail.ca>
2025-03-26 19:01:59 -0600

    - Initialize project - create game canvas with Phaser - added background image - added puck icon - added basic styling - created ability to move puck with forward and backwar arrows

---
+++ src/main.js
@@ -0,0 +1,63 @@
+import './style.css'
+import Phaser from 'phaser'
+
+const sizes = {
+  width: 1000,
+  height: 500
+}
+
+const speedDown = 300
+
+class GameScene extends Phaser.Scene{
+  constructor (){
+    super("scene-game")
+    this.player
+    this.cursor
+    this.playerSpeed=speedDown+50
+  }
+
+  preload(){
+  this.load.image("bg", "assets/bg.png")
+  this.load.image("puck", "assets/puck.png")
+  }
+
+  create(){
+    this.add.image(0,0, "bg").setOrigin(0,0)
+    this.player = this.physics.add.image(0,sizes.height-100, "puck").setOrigin(0,0)
+    this.player.setImmovable(true)
+    this.player.body.allowGravity = false
+    this.player.setCollideWorldBounds(true);
+
+    this.cursor=this.input.keyboard.createCursorKeys()
+  }
+
+  update(){
+    const {left, right} = this.cursor;
+
+    if (left.isDown) {
+      this.player.setVelocityX (-this.playerSpeed);
+    } else if (right.isDown) {
+      this.player.setVelocityX(this.playerSpeed);
+    } else {
+      this.player.setVelocityX(0);
+    }
+  }
+
+}
+const config = {
+  type: Phaser.WEBGL,
+  width: sizes.width,
+  height: sizes.height,
+  canvas: gameCanvas,
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: speedDown },
+      debug: true,
+    },
+  },
+  scene: [GameScene],
+};
+
+const game = new Phaser.Game(config);
+
